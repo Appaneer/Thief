056d0be5ec557784eddbb8b9898bc8ba25874497
xavierlu <xllgms@gmail.com>
2016-03-04 20:45:15 -0500

    updated playerMovementManager

--- Assets/Scripts/PlayerMovementManager.cs
+++ Assets/Scripts/PlayerMovementManager.cs
@@ -7,8 +7,10 @@ public class PlayerMovementManager : MonoBehaviour {
 	public float speed;
 	public GameObject player;
 	private List<Vector2> path;
+	private int currentNode;
 
 	void Start(){
+		currentNode = 0;
 		path = new List<Vector2> ();
 	}
 
@@ -20,21 +22,46 @@ public class PlayerMovementManager : MonoBehaviour {
 		#endif
 
 		#if UNITY_ANDROID
-
-		if(Input.touchCount == 0){
-			path = new List<Vector2> ();
-		}
-		else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) 
+		if (Input.touchCount > 0) 
 		{
-			path.Add(Input.GetTouch(0).position);
+			Vector2 temp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+			path.Add(temp);
+		}
+		else if(Input.touchCount == 0){
+			StartCoroutine(followPath());
 		}
 
-		if(path != null){
+
+		#endif
+	}
+
+	IEnumerator followPath(){
+		if(path.Count > 0){
 			for(int i = 0; i < path.Count-1; i++){
 				Debug.DrawLine(path[i], path[i+1]);
 			}
+			//if (Vector2.Distance (transform.position, path [currentNode]) <= 1.0f) {
+				while (currentNode < path.Count - 1) {
+					Vector2 diff = (path [currentNode] - (Vector2)transform.position);
+					float atan2 = Mathf.Atan2 (diff.y, diff.x);
+					transform.rotation = Quaternion.Euler (0f, 0f, atan2 * Mathf.Rad2Deg);
+					transform.position = Vector2.Lerp (transform.position, path [currentNode], speed);
+					if (Vector2.Distance (transform.position, path [currentNode]) <= 0.2f) {
+						currentNode++;
+					}
+					yield return new WaitForEndOfFrame ();
+				}
+			//} 
+			//else {
+			//	path = new List<Vector2> ();
+			//	currentNode = 0;
+			//}
 		}
-		#endif
+		if(currentNode >= path.Count - 1){
+			path = new List<Vector2> ();
+			currentNode = 0;
+		}
+
 	}
 
 
