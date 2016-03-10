169baf388444c1011f60942738ded2cb1e0a774a
xavierlu <xllgms@gmail.com>
2016-03-10 17:14:00 -0500

    updated navigation system

--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class Navigation : MonoBehaviour {
 
-	public GameObject target;
 	public NavMeshAgent agent;
 	private Vector3 fixedPos;
 
@@ -12,18 +11,15 @@ public class Navigation : MonoBehaviour {
 		#if UNITY_ANDROID
 		if(Input.touchCount == 1){
 			Vector3 pos = Camera.main.ScreenToWorldPoint(Input.GetTouch (0).position);
-			agant.SetDestination (pos);
-			target.transform.position = pos;
+			agent.SetDestination (pos);
 		}
 		#endif
 
 		#if UNITY_EDITOR
-		if(Input.GetMouseButton(0))
-		{
+
 		Vector3 pos1 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		agent.SetDestination (pos1);
-		target.transform.position = pos1;
-		}
+
 		#endif
 	}
 }
