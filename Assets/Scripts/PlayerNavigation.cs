85eb5ca830afb0e22e6899eb732aac65dae4ea77
xavierlu <xllgms@gmail.com>
2016-03-18 17:53:23 -0400

    added basic UI

--- Assets/Scripts/PlayerNavigation.cs
+++ Assets/Scripts/PlayerNavigation.cs
@@ -3,28 +3,24 @@ using System.Collections;
 
 public class PlayerNavigation : MonoBehaviour {
 
-	public NavMeshAgent agent;
+	public static NavMeshAgent agent;
 
 	// Use this for initialization
 	void Start () {
-		
+		agent = this.GetComponent<NavMeshAgent> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		#if UNITY_ANDROID
-		if(Input.touchCount == 1){
-			Vector3 pos = Camera.main.ScreenToWorldPoint(Input.GetTouch (0).position);
-			agent.SetDestination (pos);
-		}
+		if(Input.touchCount == 1)
+			agent.SetDestination (Camera.main.ScreenToWorldPoint(Input.GetTouch (0).position));
 		#endif
 
 		#if UNITY_EDITOR
-
-		if(Input.GetMouseButton(0))
-		{
-			Vector3 pos1 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			agent.SetDestination (pos1);
+		if(Input.GetMouseButton(0)){
+			agent.SetDestination (Camera.main.ScreenToWorldPoint(Input.mousePosition));
+	
 		}
 		#endif
 	}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+/*
+	public Button settingButton;
+	public Button helpButton;
+	public Button infoButton;
+	public Button pauseButton;
+	public Button playButton;
+	public Button shopButton;
+	public Button resetButton;
+*/
+	public void play(){
+		SceneManager.LoadScene ("Test");
+	}
+
+}
